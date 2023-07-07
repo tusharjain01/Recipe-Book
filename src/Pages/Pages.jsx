e63cbbfcd48ce3773c1a3dad1181bc5561096c5b
Tushar Jain <75475892+tusharjain01@users.noreply.github.com>
2023-07-07 10:42:16 +0530

    Project Ready

---
+++ src/Pages/Pages.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Home from './Home'
+import Cuisine from './Cuisine'
+import Searched from './Searched'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Recipe from './Recipe'
+import { AnimatePresence } from 'framer-motion'
+
+function Pages() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path='/cuisine/:type' element={<Cuisine />} />
+        <Route path='/searched/:search' element={<Searched />}></Route>
+        <Route path='/recipe/:id' element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+
+
+export default Pages
